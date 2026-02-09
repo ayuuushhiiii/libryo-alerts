edc737010bc2d5c96993c9c746f2538e9d14ee8b
Ayushi Saxena <Ayushi.Saxena@insight.com>
2026-02-09 16:01:36 +0530

    adding all files to github

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+module "sns" {
+  source     = "./modules/sns"
+  topic_name = "lambda-log-alerts"
+  email      = var.alert_email
+}
+
+module "log_filters" {
+  source = "./modules/log-metric-filter"
+
+  for_each = toset(var.lambda_names)
+
+  lambda_name      = each.key
+  filter_pattern   = "ERROR OR Exception OR INIT_REPORT OR \"Task timed out\""
+  metric_name      = "ErrorCount"
+  metric_namespace = "Lambda/LogMonitoring"
+}
+
+module "alarms" {
+  source = "./modules/alarm"
+
+  for_each = toset(var.lambda_names)
+
+  alarm_name       = "${each.key}-error-alarm"
+  metric_name      = module.log_filters[each.key].metric_name
+  metric_namespace = module.log_filters[each.key].metric_namespace
+  sns_topic_arn    = module.sns.topic_arn
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "lambda_names" {
+  type = list(string)
+}
+
+variable "alert_email" {
+  type = string
+}
